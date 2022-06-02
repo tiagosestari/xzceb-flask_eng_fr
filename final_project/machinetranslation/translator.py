08222272a0b0fc3e9a1b15e03e2e545764265751
Tiago Sestari <tiagosestari@gmail.com>
2022-06-02 15:40:25 +0000

    Created Machinetrasnlation module and imported to be executed on the endpoints.

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,32 @@
+import os
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def english_to_french(english_text):
+    """Method for english to french translation."""
+    french_text = language_translator.translate(
+    text=english_text,
+    model_id='en-fr').get_result()
+    return french_text['translations'][0]['translation']
+
+def french_to_english(french_text):
+    """Method for french to english translation."""
+    english_text = language_translator.translate(
+    text=french_text,
+    model_id='fr-en').get_result()
+    return english_text['translations'][0]['translation']
